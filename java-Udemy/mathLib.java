9596fe1dac2b0868f68304151828c9a18c49d4a8
unknown <dreicleal@hotmail.com>
2023-01-06 16:33:18 -0300

    adding bhaskara example

--- java-Udemy/mathLib.java
+++ java-Udemy/mathLib.java
@@ -20,5 +20,21 @@ public class mathLib  {
     System.out.printf("A potência de %.1f elevado a %.1f é igual a %.1f%n", x, y, A);
     System.out.printf("A potência de %.1f elevado a %.1f é igual a %.1f%n", y, x, B);
     System.out.printf("A potência de %.1f elevado a %.1f é igual a %.1f%n", z, x, C);
+
+    // fórmula de Bhaskara
+    
+    double a, b, c ,X1, X2, delta;
+    a = 2;
+    b = 12;
+    c = 2;
+    
+    delta = Math.pow(b, 2) -4.0*a*c; 
+
+    X1 = (-b + Math.sqrt(delta))/(2.0*a);
+    X2 = (-b - Math.sqrt(delta))/(2.0*a);
+
+    System.out.printf("Os resultados da equação são para X1: %.2f e para X2: %.2f%n", X1, X2);
+
+
     }
 }
